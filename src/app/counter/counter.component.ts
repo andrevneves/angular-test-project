fbab1c8273810592eb38ac5ea72af68db37df298
Andre Neves <neves.andrev@gmail.com>
2024-12-08 19:11:58 -0300

    error no-cond-assign

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -9,8 +9,9 @@ export class CounterComponent {
   counter = 0;
 
   increment() {
-    const x = 1;
-    if (x == 1) {
+    // const s = 'M'
+    const x = 1
+    if (x = 1) {
       console.log("1")
     }
     this.counter++;
