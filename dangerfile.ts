7ddbd9deed6e8c5cce64709745755d9b20ea37b5
Andre Neves <neves.andrev@gmail.com>
2024-12-09 08:01:10 -0300

    uses semver to determine the semanthic angular version.

--- dangerfile.ts
+++ dangerfile.ts
@@ -7,6 +7,7 @@
 // }
 import { danger, warn, fail } from 'danger';
 import { ESLint } from 'eslint';
+import semver from 'semver';
 
 // Exemplo: Verificar se arquivos importantes foram modificados
 if (!danger.git.modified_files.includes('README.md')) {
@@ -47,23 +48,45 @@ async function lint() {
   }
 
   // Verificar a versão mínima do Angular
-  const packageJsonContent = await danger.github.utils.fileContents('package.json');
-  if (packageJsonContent) {
-    const pkg = JSON.parse(packageJsonContent);
-    const angularVersion = pkg.dependencies['@angular/core'];
-    const versionMatch = angularVersion.match(/(\d+)\.(\d+)\.(\d+)/);
-    console.log("versionMatch:",versionMatch)
-    if (versionMatch && versionMatch.length >= 2) {
-      const major = parseInt(versionMatch[1], 10);
-      console.log("major:",major)
-      if (major < 33) {
-        warn(`A versão do Angular é ${angularVersion}. A versão mínima requerida é 33.`);
-      }
-    } else {
-      warn('Não foi possível determinar a versão do Angular.');
-    }
-  }
+  // const packageJsonContent = await danger.github.utils.fileContents('package.json');
+  // if (packageJsonContent) {
+  //   const pkg = JSON.parse(packageJsonContent);
+  //   const angularVersion = pkg.dependencies['@angular/core'];
+  //   const versionMatch = angularVersion.match(/(\d+)\.(\d+)\.(\d+)/);
+  //   console.log("versionMatch:",versionMatch)
+  //   if (versionMatch && versionMatch.length >= 2) {
+  //     const major = parseInt(versionMatch[1], 10);
+  //     console.log("major:",major)
+  //     if (major < 33) {
+  //       warn(`A versão do Angular é ${angularVersion}. A versão mínima requerida é 33.`);
+  //     }
+  //   } else {
+  //     warn('Não foi possível determinar a versão do Angular.');
+  //   }
+  // }
 
+    // Verificar a versão mínima do Angular utilizando semver
+    const packageJsonContent = await danger.github.utils.fileContents('package.json');
+    if (packageJsonContent) {
+      const pkg = JSON.parse(packageJsonContent);
+      const angularVersion = pkg.dependencies && pkg.dependencies['@angular/core'];
+  
+      if (angularVersion) {
+        // Tentar obter a versão do Angular usando semver
+        const coercedVersion = semver.coerce(angularVersion);
+  
+        if (coercedVersion) {
+          // Verificar se a versão é menor que 33.0.0
+          if (semver.lt(coercedVersion, '33.0.0')) {
+            warn(`A versão do Angular é ${angularVersion}. A versão mínima requerida é 33.`);
+          }
+        } else {
+          warn('Não foi possível determinar a versão do Angular usando semver.');
+        }
+      } else {
+        warn('Não foi encontrada a dependência @angular/core no package.json.');
+      }
+    }
 }
 
 // Executar a função lint
